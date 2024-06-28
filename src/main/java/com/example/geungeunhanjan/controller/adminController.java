67ba82c3368473c82ed227274043afb7a2f5c296
joosun0823 <sun10220466@gmail.com>
2024-06-28 16:44:43 +0900

    병합 전 커밋

--- src/main/java/com/example/geungeunhanjan/controller/adminController.java
+++ src/main/java/com/example/geungeunhanjan/controller/adminController.java
@@ -2,20 +2,22 @@ package com.example.geungeunhanjan.controller;
 
 import com.example.geungeunhanjan.domain.dto.board.BoardDTO;
 import com.example.geungeunhanjan.domain.dto.board.LifeUserInfoDTO;
+import com.example.geungeunhanjan.domain.dto.community.InquiryDTO;
+import com.example.geungeunhanjan.domain.dto.community.InquiryPagingDTO;
+import com.example.geungeunhanjan.domain.dto.inquiryPage.InquiryCriteria;
+import com.example.geungeunhanjan.domain.dto.inquiryPage.InquiryPage;
 import com.example.geungeunhanjan.domain.dto.lifePage.Criteria;
 import com.example.geungeunhanjan.domain.dto.lifePage.Page;
 import com.example.geungeunhanjan.domain.vo.board.BoardVO;
 import com.example.geungeunhanjan.domain.vo.file.UserFileVO;
 import com.example.geungeunhanjan.service.MyPageService;
 import com.example.geungeunhanjan.service.admin.admin_boardListService;
+import com.example.geungeunhanjan.service.admin.admin_inquiryService;
 import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,7 +27,7 @@ import java.util.List;
 public class adminController {
 
     private final admin_boardListService adminBoardListService;
-
+    private final admin_inquiryService adminInquiryService;
     // 메인 페이지
     @GetMapping()
     public String admin() {
@@ -71,8 +73,35 @@ public class adminController {
 
     // 1:1 문의
     @GetMapping("/inquiryList")
-    public String inquiryList() {
+    public String inquiryList(InquiryCriteria inquiryCriteria, Model model) {
+
+        List<InquiryPagingDTO> inquiries = adminInquiryService.selectAdminInquiryPage(inquiryCriteria);
+
+        int total = adminInquiryService.selectAdminInquiryTotal();
+
+        InquiryPage inquiryPage = new InquiryPage(inquiryCriteria, total);
+
+        model.addAttribute("inquiries", inquiries);
+        model.addAttribute("inquiryPage", inquiryPage);
         return "/admin/dam/admin-inquiry-list";
+    }
+
+    //1:1문의 상세
+    @GetMapping("/inquiry/detail")
+    public String inquiryDetail(Long inquiryId, Model model){
+        InquiryDTO inquiryDetail = adminInquiryService.selectAdminInquiryDetail(inquiryId);
+
+        System.out.println(inquiryDetail.getInquiryId());
+        model.addAttribute("inquiryDetail", inquiryDetail);
+        return "/admin/admin_tm/admin-inquiry-detail";
+    }
+
+    @PostMapping("/inquiry/detail/response")
+    public String inquiryDetail(@RequestParam("inquiryId") Long inquiryId, @RequestParam("inquiryResponse") String inquiryResponse){
+        System.out.println("id:" + inquiryId);
+        System.out.println("response:" + inquiryResponse);
+        adminInquiryService.writeAdminResponse(inquiryResponse, inquiryId);
+        return "redirect:/admin/inquiryList";
     }
 
     // 관리자 디테일
@@ -83,4 +112,6 @@ public class adminController {
 
         return "/admin/admin_tm/admin_detail";
     }
+
+
 }
