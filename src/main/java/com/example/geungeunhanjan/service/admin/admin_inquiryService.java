f7c663aba2516c0213593df647c0cabca95fb4ea
jang1234567 <ygjang9120@gmail.com>
2024-06-28 17:10:50 +0900

    게시판 정렬

--- src/main/java/com/example/geungeunhanjan/service/admin/admin_inquiryService.java
+++ src/main/java/com/example/geungeunhanjan/service/admin/admin_inquiryService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
+
 public interface admin_inquiryService {
     List<InquiryPagingDTO> selectAdminInquiryPage(InquiryCriteria inquiryCriteria);
 
